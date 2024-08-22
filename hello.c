c90f82f128645dd8e799075b771dd4781044e6f6
Max Schessler <schessler.max@gmail.com>
2024-08-22 08:40:54 -0500

    added import statemnets

--- hello.c
+++ hello.c
@@ -4,6 +4,9 @@
  * on 8/22/2024
 */
 
+#include <stdlib.h>
+#include <stdio.h>
+
 int main(int argc, char **argv) {
     printf("Hello, I'm Max Schessler\n");
     printf("Major is Computer Engineering\n");
